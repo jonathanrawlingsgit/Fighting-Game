a66218d295c07b415d2df669437c31c19e38805d
Jonathan Rawlings <jonraw8@gmail.com>
2022-05-18 14:12:10 -0700

    created a player and an enemy 1

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ class Sprite {
 
     update() {
         this.draw()
-       
+
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -70,3 +71,13 @@ function animate() {
 }
 
 animate()
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+            player.velocity.x = 1
+            break
+    }
+
+    console.log(event.key)
+})
